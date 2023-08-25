8feacc48ebd24c4c6142a9c5ed05df232912d395
Daniel <daniel@gmail.com>
2023-08-25 11:58:25 +0300

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a file in /tmp/school
+
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet",
+}
